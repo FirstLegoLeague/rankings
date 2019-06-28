78b7bff2d482dede36d59ca1a0256f4c4fa00cff
Roy Shmueli <2903801+2roy999@users.noreply.github.com>
2019-06-28 14:17:26 +0300

    linting project

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-'use strict'
-/* eslint node/no-deprecated-api: 0 */
-/* eslint node/no-unsupported-features: 0 */
-
 const DEFAULT_PORT = 3002
 
 const express = require('express')
@@ -11,7 +7,7 @@ const projectVersion = require('project-version')
 const { correlationMiddleware } = require('@first-lego-league/ms-correlation')
 const { Logger, loggerMiddleware } = require('@first-lego-league/ms-logger')
 
-const rankingsRouter = require('./lib/rankings_router')
+const { rankingsRouter } = require('./lib/rankings_router')
 
 const port = process.env.PORT || DEFAULT_PORT
 
